f6439b686b301ea2d28af1f31d85d6a8d7c7f56c
Caeser <ehernandez@evistaresources.com>
2018-02-06 17:51:35 -0500

    new file:   Frosted-Isles-Notify.sln 	new file:   Frosted-Isles-Notify/Frosted-Isles-Notify.csproj 	new file:   Frosted-Isles-Notify/Frosted-Isles-Notify.csproj.user 	new file:   Frosted-Isles-Notify/Program.cs 	new file:   Frosted-Isles-Notify/Properties/AssemblyInfo.cs 	new file:   Frosted-Isles-Notify/Properties/Resources.Designer.cs 	new file:   Frosted-Isles-Notify/Properties/Resources.resx 	new file:   Frosted-Isles-Notify/Properties/Settings.Designer.cs 	new file:   Frosted-Isles-Notify/Properties/Settings.settings 	new file:   Frosted-Isles-Notify/Window.Designer.cs 	new file:   Frosted-Isles-Notify/Window.cs 	new file:   Frosted-Isles-Notify/Window.resx 	new file:   Frosted-Isles-Notify/bin/Debug/FI-NTF_WKR.exe 	new file:   Frosted-Isles-Notify/bin/Debug/FI-NTF_WKR.pdb 	new file:   Frosted-Isles-Notify/bin/Debug/FI-NTF_WKR.vshost.exe 	new file:   Frosted-Isles-Notify/bin/Debug/FI-NTF_WKR.vshost.exe.manifest 	new file:   Frosted-Isles-Notify/bin/Debug/Frosted-Isles-Notify.exe 	new file:   Frosted-Isles-Notify/bin/Debug/Frosted-Isles-Notify.pdb 	new file:   Frosted-Isles-Notify/bin/Debug/Frosted-Isles-Notify.vshost.exe 	new file:   Frosted-Isles-Notify/bin/Debug/Frosted-Isles-Notify.vshost.exe.manifest 	new file:   Frosted-Isles-Notify/obj/x86/Debug/DesignTimeResolveAssemblyReferences.cache 	new file:   Frosted-Isles-Notify/obj/x86/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   Frosted-Isles-Notify/obj/x86/Debug/FI-NTF_WKR.csproj.FileListAbsolute.txt 	new file:   Frosted-Isles-Notify/obj/x86/Debug/FI-NTF_WKR.csproj.GenerateResource.Cache 	new file:   Frosted-Isles-Notify/obj/x86/Debug/FI-NTF_WKR.csprojResolveAssemblyReference.cache 	new file:   Frosted-Isles-Notify/obj/x86/Debug/FI-NTF_WKR.exe 	new file:   Frosted-Isles-Notify/obj/x86/Debug/FI-NTF_WKR.pdb 	new file:   Frosted-Isles-Notify/obj/x86/Debug/FI_NTF_WKR.Properties.Resources.resources 	new file:   Frosted-Isles-Notify/obj/x86/Debug/FI_NTF_WKR.Window.resources 	new file:   Frosted-Isles-Notify/obj/x86/Debug/Frosted-Isles-Notify.csproj.FileListAbsolute.txt 	new file:   Frosted-Isles-Notify/obj/x86/Debug/Frosted-Isles-Notify.csproj.GenerateResource.Cache

---
+++ Frosted-Isles-Notify/Window.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace FI_NTF_WKR
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Window));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 0);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Window";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Frosted-Isles-Notify/Window.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace FI_NTF_WKR
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void notify(String title, String description, int timeout)
+        {
+            timeout *= 1000;
+
+            var notification = new System.Windows.Forms.NotifyIcon()
+            {
+                Visible = true,
+                Icon = System.Drawing.SystemIcons.Information,
+                BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info,
+                BalloonTipTitle = title,
+                BalloonTipText = description,
+            };
+            notification.ShowBalloonTip(timeout);
+            Thread.Sleep(timeout);
+            notification.Dispose();
+        }
+    }
+}
